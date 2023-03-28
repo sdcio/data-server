0e5a7f6af40523d3c7bbcd46627c9662cd1e2490
Karim Radhouani <medkarimrdi@gmail.com>
2023-03-27 21:12:27 -0700

    add client set request with json file

--- client/cmd/data_set.go
+++ client/cmd/data_set.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 	"strings"
 
 	schemapb "github.com/iptecharch/schema-server/protos/schema_server"
@@ -19,6 +20,11 @@ var updates []string
 var replaces []string
 var deletes []string
 
+var updatePath string
+var replacePath string
+var updateFile string
+var replaceFile string
+
 // dataSetCmd represents the set command
 var dataSetCmd = &cobra.Command{
 	Use:          "set",
@@ -28,55 +34,9 @@ var dataSetCmd = &cobra.Command{
 		if candidate == "" {
 			return errors.New("a candidate datastore name must be specified with --candidate")
 		}
-		req := &schemapb.SetDataRequest{
-			Name: datastoreName,
-			Datastore: &schemapb.DataStore{
-				Type: schemapb.Type_CANDIDATE,
-				Name: candidate,
-			},
-		}
-		for _, upd := range updates {
-			updSplit := strings.SplitN(upd, ":::", 2)
-			if len(updSplit) != 2 {
-				return fmt.Errorf("malformed update %q", upd)
-			}
-			updPath, err := utils.ParsePath(updSplit[0])
-			if err != nil {
-				return err
-			}
-			req.Update = append(req.Update, &schemapb.Update{
-				Path: updPath,
-				Value: &schemapb.TypedValue{
-					Value: &schemapb.TypedValue_StringVal{
-						StringVal: updSplit[1],
-					},
-				},
-			})
-		}
-		for _, rep := range replaces {
-			repSplit := strings.SplitN(rep, ":::", 2)
-			if len(repSplit) != 2 {
-				return fmt.Errorf("malformed replace %q", rep)
-			}
-			repPath, err := utils.ParsePath(repSplit[0])
-			if err != nil {
-				return err
-			}
-			req.Replace = append(req.Replace, &schemapb.Update{
-				Path: repPath,
-				Value: &schemapb.TypedValue{
-					Value: &schemapb.TypedValue_StringVal{
-						StringVal: repSplit[1],
-					},
-				},
-			})
-		}
-		for _, del := range deletes {
-			delPath, err := utils.ParsePath(del)
-			if err != nil {
-				return err
-			}
-			req.Delete = append(req.Delete, delPath)
+		req, err := buildDataSetRequest()
+		if err != nil {
+			return err
 		}
 		ctx, cancel := context.WithCancel(cmd.Context())
 		defer cancel()
@@ -101,4 +61,104 @@ func init() {
 	dataSetCmd.Flags().StringArrayVarP(&updates, "update", "", []string{}, "update path and value separated by a ':::'")
 	dataSetCmd.Flags().StringArrayVarP(&replaces, "replace", "", []string{}, "replace path and value separated by a ':::'")
 	dataSetCmd.Flags().StringArrayVarP(&deletes, "delete", "", []string{}, "delete path")
+	//
+	dataSetCmd.Flags().StringVarP(&replacePath, "replace-path", "", "", "replace path to be used with --replace-file")
+	dataSetCmd.Flags().StringVarP(&updatePath, "update-path", "", "", "update path to be used with --update-file")
+	dataSetCmd.Flags().StringVarP(&replaceFile, "replace-file", "", "", "replace file containing a JSON or JSON_IETF value")
+	dataSetCmd.Flags().StringVarP(&updateFile, "update-file", "", "", "update file containing a JSON or JSON_IETF value")
+}
+
+func buildDataSetRequest() (*schemapb.SetDataRequest, error) {
+	req := &schemapb.SetDataRequest{
+		Name: datastoreName,
+		Datastore: &schemapb.DataStore{
+			Type: schemapb.Type_CANDIDATE,
+			Name: candidate,
+		},
+		Update:  make([]*schemapb.Update, 0),
+		Replace: make([]*schemapb.Update, 0),
+		Delete:  make([]*schemapb.Path, 0),
+	}
+	if updatePath != "" {
+		updPath, err := utils.ParsePath(updatePath)
+		if err != nil {
+			return nil, err
+		}
+		b, err := os.ReadFile(updateFile)
+		if err != nil {
+			return nil, err
+		}
+		req.Update = append(req.Update, &schemapb.Update{
+			Path: updPath,
+			Value: &schemapb.TypedValue{
+				Value: &schemapb.TypedValue_JsonVal{
+					JsonVal: b,
+				},
+			},
+		})
+	} else {
+		for _, upd := range updates {
+			updSplit := strings.SplitN(upd, ":::", 2)
+			if len(updSplit) != 2 {
+				return nil, fmt.Errorf("malformed update %q", upd)
+			}
+			updPath, err := utils.ParsePath(updSplit[0])
+			if err != nil {
+				return nil, err
+			}
+			req.Update = append(req.Update, &schemapb.Update{
+				Path: updPath,
+				Value: &schemapb.TypedValue{
+					Value: &schemapb.TypedValue_StringVal{
+						StringVal: updSplit[1],
+					},
+				},
+			})
+		}
+	}
+	if replacePath != "" {
+		repPath, err := utils.ParsePath(replacePath)
+		if err != nil {
+			return nil, err
+		}
+		b, err := os.ReadFile(replaceFile)
+		if err != nil {
+			return nil, err
+		}
+		req.Replace = append(req.Replace, &schemapb.Update{
+			Path: repPath,
+			Value: &schemapb.TypedValue{
+				Value: &schemapb.TypedValue_JsonVal{
+					JsonVal: b,
+				},
+			},
+		})
+	} else {
+		for _, rep := range replaces {
+			repSplit := strings.SplitN(rep, ":::", 2)
+			if len(repSplit) != 2 {
+				return nil, fmt.Errorf("malformed replace %q", rep)
+			}
+			repPath, err := utils.ParsePath(repSplit[0])
+			if err != nil {
+				return nil, err
+			}
+			req.Replace = append(req.Replace, &schemapb.Update{
+				Path: repPath,
+				Value: &schemapb.TypedValue{
+					Value: &schemapb.TypedValue_StringVal{
+						StringVal: repSplit[1],
+					},
+				},
+			})
+		}
+	}
+	for _, del := range deletes {
+		delPath, err := utils.ParsePath(del)
+		if err != nil {
+			return nil, err
+		}
+		req.Delete = append(req.Delete, delPath)
+	}
+	return req, nil
 }
