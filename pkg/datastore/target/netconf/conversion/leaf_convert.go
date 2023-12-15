a578e3e194c4ee1bbe325e3083ee8f03faba029b
Karim Radhouani <medkarimrdi@gmail.com>
2023-12-14 22:22:02 -0800

    allow setting include-ns though RPC when creating a netconf datastore

--- pkg/datastore/target/netconf/conversion/leaf_convert.go
+++ pkg/datastore/target/netconf/conversion/leaf_convert.go
@@ -42,6 +42,9 @@ func Convert(value string, lst *sdcpb.SchemaLeafType) (*sdcpb.TypedValue, error)
 	case "empty":
 		// TODO https://www.rfc-editor.org/rfc/rfc6020.html#section-9.11
 		// I'm not sure what should be returned atm.
+		// KR:use an empty string for a leaf of type "empty"
+		//    probably need to add typedValue of type Empty in the protos
+		return ConvertString(value, lst)
 	case "bits":
 	// TODO: https://www.rfc-editor.org/rfc/rfc6020.html#section-9.7
 	case "binary": // https://www.rfc-editor.org/rfc/rfc6020.html#section-9.8
