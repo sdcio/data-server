d2721f750aa33f2fe1da54d0565154776e5ce8ba
Alexander North <alexandernorth@users.noreply.github.com>
2025-07-22 09:24:26 +0200

    resolved TODO regarding namespacing of key elements

--- pkg/tree/xml.go
+++ pkg/tree/xml.go
@@ -285,8 +285,6 @@ func xmlAddKeyElements(s Entry, parent *etree.Element) {
 			// and finally we create the key elements in schema order
 			keyElem := etree.NewElement(schemaKeys[i])
 			keyElem.SetText(treeElem.PathName())
-			//TODO: Do we need to add namespaces?
-			// xmlAddNamespaceConditional(parentSchema, parentSchema.GetParent(), keyElem, true)
 			parent.InsertChildAt(0, keyElem) // we go backwards, so always add to front of parent
 			treeElem = treeElem.GetParent()
 		}
